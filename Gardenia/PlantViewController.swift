5a1cbb70f3f189cceeeb3500cd222dfab0a93f89
Aayam Sharma <aayamcoding@icloud.com>
2021-10-20 18:28:55 -0500

    First attempt at basic mockup. Temporary images used. Will need to update (probably along with everything else) to match overall visual theme.

--- Gardenia/PlantViewController.swift
+++ Gardenia/PlantViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class PlantViewController: UIViewController {
+    
+    var totalFert:Float = 0.1
+    var fertProg:Float = 1.0
+    
+    @IBOutlet var mainPlant: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainPlant.image = UIImage(named: "modelPlantSmall")
 
         // Do any additional setup after loading the view.
     }
@@ -25,5 +31,19 @@ class PlantViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBOutlet var fertProgress: UIProgressView!
+    
+    @IBAction func fertelize(){
+        fertProg = fertProg - 0.3
+        totalFert += 0.3
+        if totalFert >= 0.9{
+            mainPlant.image = UIImage(named: "modelPlantLarge")
+        }
+        else if totalFert >= 0.45{
+            mainPlant.image = UIImage(named: "modelPlantMedium")
+        }
+        fertProgress.setProgress(fertProg, animated: true)
+        super.viewDidLoad()
+    }
 }
