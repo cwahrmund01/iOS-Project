1dcb67d8fba8b07a3c897f67e472ea79df597ca8
Cedric Wahrmund <cedricwahrmund@gmail.com>
2021-10-27 21:06:08 -0500

    Fixed UI stuff and added buttons to allow for buying and selling

--- Gardenia/ShopViewController.swift
+++ Gardenia/ShopViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-class ShopViewController: UIViewController {
-
+class ShopViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet var strawberryButton: UIButton!
+    @IBOutlet var windowButton: UIButton!
+    @IBOutlet var sellHarvestButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        strawberryButton.titleLabel?.lineBreakMode = .byClipping
+        strawberryButton.titleLabel?.numberOfLines = 1
+        windowButton.titleLabel?.lineBreakMode = .byClipping
+        windowButton.titleLabel?.numberOfLines = 1
+        sellHarvestButton.titleLabel?.lineBreakMode = .byClipping
+        sellHarvestButton.titleLabel?.numberOfLines = 1
     }
-    
 
+    
 }
