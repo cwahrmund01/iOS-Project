ea32e1982a054fb08e5bcd7b0a90cf96d61f7c6f
Cedric Wahrmund <cedricwahrmund@gmail.com>
2021-10-27 21:10:10 -0500

    Added button actions, button labels still break once buttons are pressed though

--- Gardenia/ShopViewController.swift
+++ Gardenia/ShopViewController.swift
@@ -11,6 +11,7 @@ class ShopViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet var strawberryButton: UIButton!
     @IBOutlet var windowButton: UIButton!
     @IBOutlet var sellHarvestButton: UIButton!
+    @IBOutlet var moneyLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +23,36 @@ class ShopViewController: UIViewController, UIGestureRecognizerDelegate {
         sellHarvestButton.titleLabel?.numberOfLines = 1
     }
 
+    @IBAction func blueberryBuy(_ sender: Any) {
+    }
+    
+    @IBAction func pepperBuy(_ sender: Any) {
+    }
+    
+    @IBAction func beansBuy(_ sender: Any) {
+    }
+    
+    @IBAction func tomatoBuy(_ sender: Any) {
+    }
+    
+    @IBAction func strawberryBuy(_ sender: Any) {
+    }
+    
+    @IBAction func fertilizerBuy(_ sender: Any) {
+    }
+    
+    @IBAction func gardenBackgroundBuy(_ sender: Any) {
+    }
+    
+    @IBAction func windowBackgroundBuy(_ sender: Any) {
+    }
+    
+    @IBAction func meadowBackgroundBuy(_ sender: Any) {
+    }
+    
+    @IBAction func sellAllHarvestables(_ sender: Any) {
+        moneyLabel.text = "123"
+    }
+    
     
 }
