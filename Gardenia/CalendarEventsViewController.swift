b0cd39a28970b127b0cb2d7be69d39c4ea182a6f
Luisa E. Espinoza <lee.deleon99@hotmail.com>
2021-11-10 19:19:43 -0600

    Trying to use Storyboard reference

--- Gardenia/CalendarEventsViewController.swift
+++ Gardenia/CalendarEventsViewController.swift
@@ -6,48 +6,77 @@
 //
 
 import UIKit
-
-calendarList = ["date1", "date2"]
+import EventKitUI
+import EventKit
 
 class CalendarEventsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
     @IBOutlet weak var tableView: UITableView!
     
+    var calendar: EKCalendar!
+    var events: [EKEvent]?
     let textCellIdentifier = "TextCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.title = "Calendar Events"
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        let dateFormatter = DateFormatter()
+         dateFormatter.dateFormat = "yyyy-MM-dd"
+         
+         // Create start and end date NSDate instances to build a predicate for which events to select
+         let startDate = dateFormatter.date(from: "2022-01-01")
+         let endDate = dateFormatter.date(from: "2021-08-31")
+         
+         if let startDate = startDate, let endDate = endDate {
+             let eventStore = EKEventStore()
+             
+             // Use an event store instance to create and properly configure an NSPredicate
+             let eventsPredicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: [calendar!])
+             
+             // Use the configured NSPredicate to find and return events in the store that match
+             self.events = eventStore.events(matching: eventsPredicate).sorted(){
+                 (e1: EKEvent, e2: EKEvent) -> Bool in
+                 return e1.startDate.compare(e2.startDate) == ComparisonResult.orderedAscending
+             }
+         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calendarList.count
+        return events!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
-        let row - indexPath.row
-        cell.textLabel?.text = "\(calendarList[row])"
+        let row = indexPath.row
+        cell.textLabel?.text = "\(events![row])"
         return cell
     }
-    
-    // Swiping and deleting, but it also has to delete from calendar view. may need to use core data
-    // can use pizza assignment
-    
-        
-        // Swiping and deleting
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        self.event = EKEventStore()
+//        self.reminder = [EKReminder]()
+//        self.eventStore.requestAccess(to: EKEntityType.reminder) { (granted, error) in
+//            if granted{
+//                let predicate = self.eventStore.predicateForReminders(in: nil)
+//                self.eventStore.fetchReminders(matching: predicate, completion: { reminders in
+//                    guard let reminders = reminders else {
+//                        return
+//                    }
+//                    self.reminders = reminders
+//                    DispatchQueue.main.async {
+//                        self.tableView.reloadData()
+//                    }
+//                })
+//            } else {
+//                print("The app is not permitted to access reminders, make sure to grant permission in the settings and try again")
+//            }
+//        }
+//    }
 
 }
--- Gardenia/CalendarViewController.swift
+++ Gardenia/CalendarViewController.swift
@@ -6,34 +6,56 @@
 //
 
 import UIKit
-import FSCalendar
+import EventKitUI
+import EventKit
 
-class CalendarViewController: UIViewController, FSCalendarDelegate {
-
-    @IBOutlet var calendar: FSCalendar!
+class CalendarViewController: UIViewController, EKEventViewDelegate {
+    
+    let storedEvents = EKEventStore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        calendar.delegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
         // Do any additional setup after loading the view.
     }
     
-    
-//    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
-//        let formatter = DataFormatter()
-//        formatter.dateFormat = "EEEE MM-dd-YYYY"
-//        let string = formatter.string(from: date)
-//        print("\(string)")
-//    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func didTapAdd() {
+//        let vc = EKCalendarChooser()
+//        vc.showsDoneButton = true
+//        vc.showsCancelButton = true
+//        vc.delegate = self
+//        present(vc, animated: true)
+        
+        storedEvents.requestAccess(to: .event) { [weak self] success, error in
+            if success, error == nil {
+                DispatchQueue.main.async {
+                    
+                    guard let storedEvents = self?.storedEvents else {return}
+                    
+                    let newEvent = EKEvent(eventStore: storedEvents)
+                    newEvent.title = "Reminder to water your plants"
+                    newEvent.startDate = Date()
+                    newEvent.endDate = Date()
+                    
+                    let otherVC = EKEventEditViewController()
+                    otherVC.eventStore = storedEvents
+                    otherVC.event = newEvent
+                    self?.present(otherVC, animated: true, completion: nil)
+                    
+//                    let vc = EKEventViewController()
+//                    vc.delegate = self
+//                    vc.event = newEvent
+//                    let navVC = UINavigationController(rootViewController: vc)
+//                    self?.present(vc, animated: true)
+                }
+            }
+        }
     }
-    */
+    
+    func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+
 
 }
